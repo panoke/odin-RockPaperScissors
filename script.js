6454865f09ccec249c92d9d6e75ac4810ca0f565
panoke <3214736+panoke@users.noreply.github.com>
2026-09-04 15:21:53 +0100

    Add function to return random choice of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+// function to return random choice of rock, paper, scissors
+function getComputerChoice () {
+    randomValue = Math.floor(Math.random() * 3) + 1;
+    switch (randomValue) {
+        case 1: 
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3: 
+            return 'scissors'
+            break; 
+        }
+}
